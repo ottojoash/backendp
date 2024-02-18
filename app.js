da293c2303a393a5de5a4599254e5d1fe4c78361
ottojoash <34677630+ottojoash@users.noreply.github.com>
2024-02-18 14:09:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('/api/sendEmail', async (req, res) => {
     // Create email message
     const message = {
       from: process.env.OUTLOOK_EMAIL,
-      to: 'info@penielbeachotel.com , louisjoshbricks@gmail.com', // Email address where the booking information will be sent
+      to: 'info@penielbeachotel.com', // Email address where the booking information will be sent
       subject: 'Booking Information',
       text: `
         Check-in: ${checkIn}
@@ -65,7 +65,7 @@ app.post('/api/sendMessage', async (req, res) => {
     // Create email message
     const emailMessage = {
       from: process.env.OUTLOOK_EMAIL, // Sender address
-      to: 'info@penielbeachotel.com , penielbeachhotel@gmail.com', // Replace with your receiving email address
+      to: 'info@penielbeachotel.com', // Replace with your receiving email address
       subject: `Message from ${email}`,
       text: `
         Email: ${email}
