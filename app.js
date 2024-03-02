cc32cf3d726dd33e8146b6ab275a07f2a888813d
ottojoash <louisjoshbricks@gmail.com>
2024-03-02 10:31:29 +0300

    email

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/api/sendEmail', async (req, res) => {
     // Create email message
     const message = {
       from: process.env.OUTLOOK_EMAIL,
-      to: 'info@penielbeachotel.com , penielbeachhotel@gmail.com', // Email address where the booking information will be sent
+      to: 'info@penielbeachotel.com , penielbeachhotel@gmail.com, kabuyerosette@yahoo.co.uk', // Email address where the booking information will be sent
       subject: 'Booking Information',
       text: `
         Name: ${names}
